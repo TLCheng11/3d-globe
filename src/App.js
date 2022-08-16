4680d33a4093ec8581f889705398061582f50342
Ting Luen Cheng <scorpio_ctl@hotmail.com>
2022-08-15 22:35:28 -0400

    added selection and color change to globe

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import World from './World';
 
 function App() {
+  const [countryName, setCountryName] = useState("")
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Pick a country you want to visit: {countryName}</h1>
+      </div>
+      <div>
+        <World setCountryName={setCountryName}/>
+      </div>
     </div>
   );
 }
---
+++ src/World.js
@@ -0,0 +1,56 @@
+import { useEffect, useRef, useState } from 'react';
+import Globe from 'react-globe.gl';
+
+function World({setCountryName}) {
+  const globeEl = useRef();
+  const [countries, setCountries] = useState({ features: []});
+  const [altitude, setAltitude] = useState(0.1);
+  const [color, setColor] = useState(() => feat => 'rgba(200, 0, 0, 0.6)')
+  const [transitionDuration, setTransitionDuration] = useState(1000);
+
+  useEffect(() => {
+    // load data
+    fetch('./datasets/ne_110m_admin_0_countries.geojson')
+      .then(res => res.json())
+      .then(countries=> {
+        console.log(countries)
+        setCountries(countries);
+
+        setTimeout(() => {
+          setTransitionDuration(4000);
+          setAltitude(0.1);
+        }, 3000);
+      });
+  }, []);
+
+  useEffect(() => {
+    // Auto-rotate
+    globeEl.current.controls().autoRotate = true;
+    globeEl.current.controls().autoRotateSpeed = 0.3;
+
+    globeEl.current.pointOfView({ altitude: 4 }, 5000);
+  }, []);
+
+  return <Globe
+    ref={globeEl}
+    globeImageUrl="//unpkg.com/three-globe/example/img/earth-dark.jpg"
+
+    polygonsData={countries.features.filter(d => d.properties.ISO_A2 !== 'AQ')}
+    polygonAltitude={altitude}
+    polygonCapColor={color}
+    polygonSideColor={() => 'rgba(0, 100, 0, 0.15)'}
+    polygonLabel={({ properties: d }) => `
+      <b>${d.ADMIN} (${d.ISO_A2})</b> <br />
+    `}
+    polygonsTransitionDuration={transitionDuration}
+    onPolygonClick={({ __id: id, properties: d }) => {
+      setCountryName(d.ADMIN)
+      setAltitude(() => feat => id === feat.__id ? 0.3 : 0.1)
+    }}
+    onPolygonHover={(e) => {
+      setColor(() => feat => e.__id === feat.__id ? 'rgba(36, 36, 177, 0.8)' :'rgba(200, 0, 0, 0.6)')
+    }}
+  />;
+}
+
+export default World;
